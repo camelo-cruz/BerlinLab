eef783a9ec96727fed765ac180c13ba86ed8a1f1
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2024-01-08 20:47:09 +0100

    updated textgrids

--- audio_cut/create_timestamps.py
+++ audio_cut/create_timestamps.py
@@ -2,54 +2,56 @@
 """
 Created on Wed Jun 21 13:23:57 2023
 
-@author: emanu
+@author: emanuele de Rossi - Universität Potsdam
 """
+
+import argparse
 
 from praatio import textgrid
 from praatio.utilities.constants import Interval
 
 import json 
 
-with open("input.json", "r", encoding = "utf-8") as jsonfile:
+def main():
+    parser = argparse.ArgumentParser(
+        description='Train LSTM'
+    )
     
-    speakonly = json.load(jsonfile)
+    parser.add_argument(
+        'input'
+    )
     
+    args = parser.parse_args()
+    
+    with open(args.input, "r", encoding = "utf-8") as jsonfile:
+        speakonly = json.load(jsonfile)
+    
+    # create an empty textgrid
+    tg = textgrid.Textgrid()
+    
+    # create a tier to store all the words
+    wordTier = textgrid.IntervalTier(name = "words: ", entries = [], minT = 0, maxT = 1000)
+    
+    all_words = dict()
+    #idx = 1
+    for segment in speakonly["segments"]:
+        for word in segment["words"]:
+            print(word["word"], word["start"], word["end"])
+            all_words[word["word"]] = (word["start"], word["end"])
+            
+    
+            if "start" in word:
+                interval = Interval(start = word["start"], end = word["end"], label = word["word"]) 
+                wordTier.insertEntry(interval)
+            
+            
+    # add the tier to the textgrid
+    tg.addTier(wordTier)
+    
+    output = args.input.replace('.json', '.TextGrid')
+    
+    
+    tg.save(output, format="long_textgrid", includeBlankSpaces=True, reportingMode = "silence")
 
-# create an empty textgrid
-tg = textgrid.Textgrid()
-
-# create a tier to store all the words
-wordTier = textgrid.IntervalTier(name = "words: ", entries = [], minT = 0, maxT = 10000)
-
-all_words = dict()
-#idx = 1
-for segment in speakonly["segments"]:
-    for word in segment["words"]:
-        print(word["word"], word["start"], word["end"])
-        all_words[word["word"]] = (word["start"], word["end"])
-        
-        # create the intervals for the tier
-        
-        # IF YOU USE WHISPER_TIMESTAMPED, UNCOMMENT THIS LINE AND COMMENT THE 3 LINES BELOW
-        # interval = Interval(start = word["start"], end = word["end"], label = word["text"], )
-        # wordTier.insertEntry(interval)
-        
-        
-        # IF YOU USE WHISPERX, USE THESE 3 LINES INSTEAD
-        if "start" in word:
-            interval = Interval(start = word["start"], end = word["end"], label = word["word"]) 
-            wordTier.insertEntry(interval)
-        
-        
-        
-# add the tier to the textgrid
-
-# tg.addTier(wordTier)
-
-
-#print(tg.wordTier.entries)
-        
-#print(tg.tierNames)
-# save the textgrid
-
-tg.save("output.TextGrid", format="long_textgrid", includeBlankSpaces=True, reportingMode = "silence")
+if __name__ == '__main__':
+    main()
