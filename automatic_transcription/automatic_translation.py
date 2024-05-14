8674d9114941e17f92e26c4f2e2e94492ec94637
Alejandra Camelo Cruz <135133679+camelo-cruz@users.noreply.github.com>
2024-05-14 18:18:29 +0200

    Arne (#55)
    * roman alphabet
    
    * Addition of transliteration option
    
    Addition of transliteration option
    
    ---------
    
    Co-authored-by: arnegoelz <149581311+arnegoelz@users.noreply.github.com>

--- automatic_transcription/automatic_translation.py
+++ automatic_transcription/automatic_translation.py
@@ -7,14 +7,18 @@ def translate(file, target_lang):
     df = pd.read_excel(file)
     
     #read the excel sheet and either add a new column 'automatic_translation'
-    #or transform add to the existing column the translations
+    #or add to the existing column the translations
     for i in range(len(df)):
-        if 'automatic_translation' in df.columns:
-            df['automatic_translation'][i] = GoogleTranslator(source='auto', target='en').translate(df['transcription'][i]) 
-        else:
-            df.insert(df.columns.get_loc('transcription_comments'), 'automatic_translation', 'n')
-            df['automatic_translation'][i] = GoogleTranslator(source='auto', target='en').translate(df['transcription'][i]) 
-    
+        try:
+            if 'automatic_translation' in df.columns:
+                df.loc[i,'automatic_translation'] = GoogleTranslator(source='auto', target='en').translate(df['transcription'][i]) 
+            else:
+                df.insert(df.columns.get_loc('transcription_comments'), 'automatic_translation', 'n')
+                df.loc[i,'automatic_translation'] = GoogleTranslator(source='auto', target='en').translate(df['transcription'][i]) 
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            
+        
     return df
 
 
@@ -23,18 +27,21 @@ def main():
     Main function to translate a manually prepaired transcription
     
     arguments:
-    file
+    directory
     target language
     """
     
     parser = argparse.ArgumentParser(description="automatic transcription")
-    parser.add_argument("input_file")
+    parser.add_argument("input_dir")
     parser.add_argument("target_language", default="en")
     args = parser.parse_args()
     
-    df = translate(args.input_file, args.target_language)
-    
-    df.to_excel(args.input_file)
+    for subdir, dir, files in os.walk(args.input_dir):
+        for file in files:
+            if file.endswith('.xlsx'):
+                file = os.path.join(subdir, file)
+                df = translate(file, args.target_language)
+                df.to_excel(file)
 
 
 if __name__ == "__main__":
