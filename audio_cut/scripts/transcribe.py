5c61be6012b0c202ded2ffc32c2ef475388be253
Alejandra Camelo Cruz <135133679+camelo-cruz@users.noreply.github.com>
2024-11-20 15:38:40 +0100

    Update transcribe.py

--- audio_cut/scripts/transcribe.py
+++ audio_cut/scripts/transcribe.py
@@ -1,7 +1,11 @@
-folder = 
+import os
+import subprocess
+
+folder = "C:\Users\camelo.cruz\Desktop\data"
 
 for filename in os.listdir(folder):
-  if filename.endswith('.wav'):
-    !whisper '{filename}' --model large --language German --word_timestamps True
-    output = filename.replace('.wav', '.json')
-    files.download(output)
+    if filename.endswith('.wav'):
+        filepath = os.path.join(folder, filename)
+        subprocess.run(['whisper', filepath, '--model', 'large', '--language', 'German', '--word_timestamps', 'True'])
+        output = filename.replace('.wav', '.json')
+        output_path = os.path.join(folder, output)
